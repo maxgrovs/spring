03b1594203c9aa491ed6e4abd04d933c07bb5b29
maxgrovs <maxgrovs@mail.ru>
2021-10-17 22:33:53 +0300

    21

---
+++ src/main/java/by/grovs/springcourse/dao/PersonDao.java
@@ -0,0 +1,32 @@
+package by.grovs.springcourse.dao;
+
+import by.grovs.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "pit"));
+        people.add(new Person(++PEOPLE_COUNT, "fil"));
+        people.add(new Person(++PEOPLE_COUNT, "sofa"));
+        people.add(new Person(++PEOPLE_COUNT, "nika"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(final int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+}
